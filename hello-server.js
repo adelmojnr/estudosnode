cc53c51d56ebd28d3f9bc6ad13978584b2c9f0bf
Adelmo Junior <junior-adelmo@hotmail.com>
2016-11-21 00:25:30 -0300

    corrigindo erros do hello-ser.js

--- hello-server.js
+++ hello-server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type" : 'text/html'});
-  response.write("<h1Hello world></h1>");
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write("<h1>Hello world</h1>");
   response.end();
 });
 server.listen(3000, function() {
