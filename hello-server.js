8649cd05a311e36af78b914f4516d4cea2df04e1
Adelmo Junior <junior-adelmo@hotmail.com>
2016-11-21 09:47:30 -0300

    Adicionando arquivo hello-rotas.js

--- hello-server.js
+++ hello-server.js
@@ -1,10 +1,17 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response){
   response.writeHead(200, {"Content-Type" : "text/html"});
   response.write("<h1>Hello world</h1>");
+  if (request.url == "/"){
+    response.write("<h1>Página principal</h1>");
+  }else if (request.url == "/bemvindo"){
+    response.write("<h1>Bem vindo: </h1>");
+  }else {
+    response.write("<h1>Página não encontrada =( </h1>");
+  }
   response.end();
 });
-server.listen(3000, function() {
+server.listen(3000, function(){
   console.log("Servidor funcionando");
 });
