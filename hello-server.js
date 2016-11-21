1d99ba427d73e70a0c22a997ab7e57a41f8e8fc5
Adelmo Junior <junior-adelmo@hotmail.com>
2016-11-21 00:02:24 -0300

    arquivo hello-world.js

---
+++ hello-server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type" : 'text/html'});
+  response.write("<h1Hello world></h1>");
+  response.end();
+});
+server.listen(3000, function() {
+  console.log("Servidor funcionando");
+});
