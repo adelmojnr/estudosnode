0c2a3def219b2707626a8b9c9b0e3f83b45ea7e0
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-04-23 17:26:50 -0300

    ES6 applied in `url-server.js`

--- url-server.js
+++ url-server.js
@@ -1,16 +1,16 @@
-var http = require('http');
-var url = require('url');
+import http from 'http';
+import url from 'url';
 
-var server = http.createServer(function(request, response){
+const server = http.createServer((request, response) => {
 	response.writeHead(200, {"Content-Type" : "text/html"});
 	response.write("<h1>Dados da query string</h1>");
-	var result = url.parse(request.url);
-	for (var key in result.query) {
-		response.writeHead("<h2>" +key+" : "+result.query[key]+"</h2>");
+	const result = url.parse(request.url);
+	for (const key in result.query) {
+		response.writeHead(`<h2>${key} : ${result.query[key]}</h2>`);
 	}
 	response.end();
 });
 
-server.listen(3000,function(){
+server.listen(3000,() => {
 	console.log("Servidor http =)");
 });
